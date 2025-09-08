569fb1885eb417eaa8df5a82cef9a76849b0fd20
rotkiV-V <120101460+rotkiV-V@users.noreply.github.com>
2025-09-08 10:34:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// generáljatok 22 számjegyű számokat 12000 darabot ezeket a számokat írjátok ki egy fájlba ez után a fájlból találjátok meg a legnagyobb és legkisebb számot
+// legkissebb és legnagyobb átlagából hány darab szám val felette és alatta
+
+Random rand = new Random();
+
+int[] szamok=new int[12000];
+
+szam();
+ string szam() 
+{
+    for (int i = 0; i < 22; i++)
+    {
+        Console.Write(rand.Next(10));
+    }
+
+}
